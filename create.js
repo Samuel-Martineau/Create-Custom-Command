2a506b0060417e175ca15ad918d2f2e9ac3c61df
Samuel-Martineau <samumartineau@gmail.com>
2020-07-30 16:02:18 -0400

    Added NVM Compatibility

--- create.js
+++ create.js
@@ -1,155 +1,153 @@
 #!/usr/bin/env node
-const { getMessage, getBin } = require("./helpers");
-const updateNotifier = require("update-notifier");
-const exec = require("child_process").execSync;
-const packageJson = require("./package.json");
-const Configstore = require("configstore");
-const isFolder = require("is-directory");
-const inquirer = require("inquirer");
-const mkdirp = require("mkdirp");
-const fs = require("fs");
-require("colors");
+const { getMessage, getBin } = require('./helpers');
+const updateNotifier = require('update-notifier');
+const exec = require('child_process').execSync;
+const packageJson = require('./package.json');
+const Configstore = require('configstore');
+const isFolder = require('is-directory');
+const inquirer = require('inquirer');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+require('colors');
 
 const config = new Configstore(packageJson.name);
 
 if (
   !(
-    config.has("language") ||
-    config.has("authorName") ||
-    config.has("commandsFolder")
+    config.has('language') ||
+    config.has('authorName') ||
+    config.has('commandsFolder')
   )
 ) {
   console.log(
     "Vous devez configurer ce paquet avant de l'utiliser. Exécutez ".red +
-      "config-cccmd".white
+      'config-cccmd'.white,
   );
   console.log(
-    "You must configure this package before using it. Execute ".red +
-      "config-cccmd".white
+    'You must configure this package before using it. Execute '.red +
+      'config-cccmd'.white,
   );
   return;
 }
 
-const user = exec("whoami")
-  .toString()
-  .trim();
+const user = exec('whoami').toString().trim();
 
 console.log();
-console.log(getMessage("welcomeCreate").blue);
+console.log(getMessage('welcomeCreate').blue);
 
 main();
 
 async function main() {
-  if (!isFolder.sync(config.get("commandsFolder")))
+  if (!isFolder.sync(config.get('commandsFolder')))
     return console.log(
-      getMessage("noCommandsDir").replace("PATH", config.get("commandsFolder"))
-        .red
+      getMessage('noCommandsDir').replace('PATH', config.get('commandsFolder'))
+        .red,
     );
   const {
     cmdName: name,
     cmdDesc: desc,
-    cmdLanguage: lang
+    cmdLanguage: lang,
   } = await inquirer.prompt([
     {
-      type: "input",
-      name: "cmdName",
-      message: getMessage("cmdName"),
+      type: 'input',
+      name: 'cmdName',
+      message: getMessage('cmdName'),
       validate(val) {
         let exists;
         try {
           fs.lstatSync(getBin() + val);
           err = true;
         } catch {}
-        if (val === "") return getMessage("emptyCmdName");
-        else if (val.includes(" ")) return getMessage("cmdNameWithSpace");
+        if (val === '') return getMessage('emptyCmdName');
+        else if (val.includes(' ')) return getMessage('cmdNameWithSpace');
         else if (/[!@#$%^&*(),.?":{}|<>/]/.test(val))
-          return getMessage("cmdNameWithSymbols");
+          return getMessage('cmdNameWithSymbols');
         else if (
           exists ||
-          fs.existsSync("/usr/bin/" + val) ||
-          fs.existsSync("/bin/" + val) ||
+          fs.existsSync('/usr/bin/' + val) ||
+          fs.existsSync('/bin/' + val) ||
           fs.existsSync(getBin() + val) ||
-          fs.existsSync(config.get("commandsFolder") + val)
+          fs.existsSync(config.get('commandsFolder') + val)
         )
-          return getMessage("cmdAlreadyExists");
+          return getMessage('cmdAlreadyExists');
         else return true;
-      }
+      },
     },
     {
-      type: "input",
-      name: "cmdDesc",
-      message: getMessage("cmdDesc"),
+      type: 'input',
+      name: 'cmdDesc',
+      message: getMessage('cmdDesc'),
       validate(val) {
-        if (val === "") return getMessage("emptyCmdDesc");
+        if (val === '') return getMessage('emptyCmdDesc');
         else return true;
-      }
+      },
     },
     {
-      type: "list",
-      name: "cmdLanguage",
-      message: getMessage("cmdLanguage"),
+      type: 'list',
+      name: 'cmdLanguage',
+      message: getMessage('cmdLanguage'),
       choices: [
         {
-          name: "JS",
-          value: "js"
+          name: 'JS',
+          value: 'js',
         },
         {
-          name: "BASH / ZSH",
-          value: "bash"
-        }
-      ]
-    }
+          name: 'BASH / ZSH',
+          value: 'bash',
+        },
+      ],
+    },
   ]);
-  const author = config.get("authorName");
+  const author = config.get('authorName');
   switch (lang) {
-    case "js":
-      console.log(getMessage("creatingCommand").green);
+    case 'js':
+      console.log(getMessage('creatingCommand').green);
       try {
-        const indexJS = `#!${getBin()}node\nrequire('colors');\n`;
+        const indexJS = `#!/usr/bin/env node\nrequire('colors');\n`;
         const packageJSON = {
           name,
-          version: "1.0.0",
+          version: '1.0.0',
           description: desc,
-          main: "index.js",
+          main: 'index.js',
           author,
-          license: "ISC"
+          license: 'ISC',
         };
-        const dirPath = `${config.get("commandsFolder")}${name}/`;
+        const dirPath = `${config.get('commandsFolder')}${name}/`;
         mkdirp.sync(dirPath);
-        fs.writeFileSync(dirPath + "index.js", indexJS);
-        fs.writeFileSync(dirPath + "package.json", JSON.stringify(packageJSON));
+        fs.writeFileSync(dirPath + 'index.js', indexJS);
+        fs.writeFileSync(dirPath + 'package.json', JSON.stringify(packageJSON));
         exec(
-          `cd ${dirPath} && npm install colors && chmod +x index.js && sudo ln -s ${dirPath}index.js ${getBin()}${name}`
+          `cd ${dirPath} && npm install colors && chmod +x index.js && sudo ln -s ${dirPath}index.js ${getBin()}${name}`,
         );
-        console.log(getMessage("cmdCreated").replace("NAME", name).green);
+        console.log(getMessage('cmdCreated').replace('NAME', name).green);
       } catch {
-        console.log(getMessage("cmdError").red);
+        console.log(getMessage('cmdError').red);
       }
       break;
-    case "bash":
-      console.log(getMessage("creatingCommand").green);
+    case 'bash':
+      console.log(getMessage('creatingCommand').green);
       try {
-        const dirPath = config.get("commandsFolder");
+        const dirPath = config.get('commandsFolder');
         mkdirp.sync(dirPath);
         fs.writeFileSync(
           dirPath + name,
-          `# ${name} (${desc}) ${getMessage("by")} ${author}`
+          `# ${name} (${desc}) ${getMessage('by')} ${author}`,
         );
         exec(
-          `cd ${dirPath} && chmod +x ${name} && sudo ln -s ${dirPath}${name} ${getBin()}${name}`
+          `cd ${dirPath} && chmod +x ${name} && sudo ln -s ${dirPath}${name} ${getBin()}${name}`,
         );
-        console.log(getMessage("cmdCreated").replace("NAME", name).green);
+        console.log(getMessage('cmdCreated').replace('NAME', name).green);
       } catch (err) {
-        console.log(err, getMessage("cmdError").red);
+        console.log(err, getMessage('cmdError').red);
       }
       break;
   }
 
   const notifier = updateNotifier({
     pkg: packageJson,
-    updateCheckInterval: 1000 * 60 * 24
+    updateCheckInterval: 1000 * 60 * 24,
   });
   if (notifier.update) {
-    console.log(getMessage("updateAvailable").green.bold);
+    console.log(getMessage('updateAvailable').green.bold);
   }
 }
--- helpers.js
+++ helpers.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-const packageJson = require("./package.json");
-const messages = require("./messages.json");
-const Configstore = require("configstore");
+const packageJson = require('./package.json');
+const messages = require('./messages.json');
+const Configstore = require('configstore');
 
 const config = new Configstore(packageJson.name);
 
 module.exports = {
   getMessage(key) {
-    return messages[key][config.get("language")];
+    return messages[key][config.get('language')];
   },
   getBin() {
-    return process.platform === "darwin" ? "/usr/local/bin/" : "/usr/bin/";
-  }
+    return process.platform === 'darwin' ? '/usr/local/bin/' : '/usr/bin/';
+  },
 };
